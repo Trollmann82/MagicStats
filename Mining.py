bd514cd629d2c5b3b3d73565d216b95a81dcd93b
Trollmann82 <32388588+Trollmann82@users.noreply.github.com>
2018-06-11 11:51:43 +0100

    Update Mining.py

--- Mining.py
+++ Mining.py
@@ -1,3 +1,4 @@
+# Imports libraries required for Python dependencies
 import requests
 import time
 import json
@@ -5,9 +6,9 @@ import datetime
 
 # Welcome text
 print("")
-print("|-----------------------------------------|")
-print("|  MagicStats v0.0.1.1 by Matz Trollmann  |")
-print("|-----------------------------------------|")
+print("|---------------------------------------|")
+print("|  MagicStats v0.0.2 by Matz Trollmann  |")
+print("|---------------------------------------|")
 print("")
 
 # Blockchain data
@@ -31,6 +32,7 @@ while True :
     # Gets API data
     url = "https://api.coinmarketcap.com/v2/ticker/2773/"
     ginresp = requests.get("https://explorer.gincoin.io/api/getnetworkhashps")
+    poolurl = "http://angrypool.com/api/walletEx?address=GW8vqADFpd1TDZh5kd89CpF3yLpRBtPUG5"
     # Calculations from API data from coin block explorer
     ginhash = float(ginresp.text)
     perchash = round(mining * gh / ginhash / 10,5)
@@ -41,10 +43,17 @@ while True :
     parsed = json.loads(data)
     price = parsed["data"]["quotes"]["USD"]["price"]
     today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # API data from pool
+    poolresponse = requests.get(poolurl)
+    data = poolresponse.text
+    parsed = json.loads(data)
+    total24htext = parsed['total']
+    last24h = round(total24htext,5)
     # Prints data to screen on chosen interval
     print(today,'The current hashrate for',gin,'is',round(ginhash / gh,4),'GH/s.')
     print(today,"Your expected hashrate of",mining,"MH/s makes out",perchash,"% of the network.")
     print(today,"Expected daily production is currently",dailycoins,gin,"per day, at an estimated value of",round(price*dailycoins,2),"USD.")
-    print(today,"-----     -----     -----     -----     -----     -----     -----     -----     -----     -----")
-
+    print(today,"You have mined",last24h,gin,"in the last 24 hours for a total value of",round(price*last24h,2),"USD")
+    print(today, "-----     -----     -----     -----     -----     -----     -----     -----     -----     -----")
     time.sleep(300)
+
