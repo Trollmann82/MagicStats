c95128054258e99c568d38667306326d92eeba33
Trollmann82 <32388588+Trollmann82@users.noreply.github.com>
2018-06-12 13:00:16 +0100

    Update and rename Mining.py to magicstats.py

--- magicstats.py
+++ magicstats.py
@@ -3,11 +3,13 @@ import requests
 import time
 import json
 import datetime
+import os
 
 # Welcome text
+os.system('clear')
 print("")
 print("|-------------------------------------------|")
-print("|    MagicStats v0.0.5 by Matz Trollmann    |")
+print("|   MagicStats v0.2.0v2 by Matz Trollmann   |")
 print("|  BTC: 3PBN9BHxFyjWoXBT1HH4YPDV5UcYBq9YsS  |")
 print("|  Github: https://github.com/Trollmann82/  |")
 print("|-------------------------------------------|")
@@ -25,16 +27,22 @@ wallet = str(input("Input your wallet address here: "))
 
 # Pool choice menu
 print("1 = Angrypool\n"
-    "2 = Bsod")
+    "2 = Bsod\n"
+    "3 = solo mining")
 poolchoice = int(input("Choose pool: "))
-
 if poolchoice == 1:
     pool = f"http://angrypool.com/api/walletEx?address="
-    poolname = str("Angrypool")
+    poolname = str("mining on Angrypool")
 if poolchoice == 2:
     pool = f"http://api.bsod.pw/api/walletEx?address="
-    poolname = str("Bsod")
-
+    poolname = str("mining on Bsod")
+if poolchoice == 3:
+    pool = str("")
+    poolname = str("solo mining")
+# Screen choice menu
+print("1 = Clear screen on every update (good for tidy information)\n"
+    "2 = Rolling data (good for being able to see historical data)")
+screenchoice = int(input("Choose behaviour: "))
 # Choosing fiat currency
 print("1 = USD\n"
       "2 = EUR\n"
@@ -53,6 +61,7 @@ mh = 1000000
 kh = 1000
 gin = 'Gincoin'
 
+os.system('clear')
 # Code to loop
 while True :
 
@@ -77,17 +86,33 @@ while True :
     price = parsed["data"]["quotes"]["USD"]["price"]
     today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # API data from pool
-    poolresponse = requests.get(poolurl)
-    data = poolresponse.text
-    parsed = json.loads(data)
-    total24htext = parsed['total']
-    last24h = round(total24htext,4)
+    if poolchoice == 1 or poolchoice == 2:
+        poolresponse = requests.get(poolurl)
+        data = poolresponse.text
+        parsed = json.loads(data)
+        total24htext = parsed['total']
+        last24h = round(total24htext,4)
+    time.sleep(5)
+    if screenchoice == 1:
+        os.system('clear')
     # Prints data to screen every 5 minutes
-    print(today," You are currently mining on ",poolname," on the address ", wallet,".",sep='')
-    print(today,'The current hashrate for',gin,'is',round(ginhash / gh,4),'GH/s.')
-    print(today,"Your expected hashrate of",mining,"MH/s makes out",perchash,"% of the network.")
-    print(today," Expected daily production is currently ", dailycoins," ",gin," per day, at an estimated value of ", round(price*dailycoins*fiat,2)," ",fiatcurr,".",sep='')
-    print(today,"You have mined",last24h,gin,"in the last 24 hours for a total value of",round(price*last24h*fiat,2),fiatcurr)
-    print(today, "-----     -----     -----     -----     -----     -----     -----     -----     -----     -----")
-    time.sleep(300)
+    print("")
+    print("|-------------------------------------------|")
+    print("|   MagicStats v0.2.0v2 by Matz Trollmann   |")
+    print("|  BTC: 3PBN9BHxFyjWoXBT1HH4YPDV5UcYBq9YsS  |")
+    print("|  Github: https://github.com/Trollmann82/  |")
+    print("|-------------------------------------------|")
+    print("")
+    print(today)
+    print("You are currently ",poolname," on the address ", wallet,".",sep='')
+    print("The current hashrate for",gin,"is",round(ginhash / gh,4),"GH/s.")
+    print("Your expected hashrate of",mining,"MH/s makes out",perchash,"% of the network.")
+    print("Expected daily production is currently ", dailycoins," ",gin," per day, at an estimated value of ", round(price*dailycoins*fiat,2)," ",fiatcurr,".",sep='')
+    if poolchoice == 1 or poolchoice == 2:
+        print("You have mined",last24h,gin,"in the last 24 hours for a total value of",round(price*last24h*fiat,2),fiatcurr)
+    if poolchoice == 3:
+        dailyblocks = round(dailycoins / reward, 3)
+        print("You will mine an estimated",dailyblocks,"blocks per day, or recieve a block every",round(24 / dailyblocks,2),"hours.")
+
+    time.sleep(295)
 
