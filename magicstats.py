968b77caeb806cce3061fc248142ca042b385586
Trollmann82 <32388588+Trollmann82@users.noreply.github.com>
2018-07-19 15:00:32 +0100

    Update magicstats.py

--- magicstats.py
+++ magicstats.py
@@ -9,7 +9,7 @@ import os
 os.system('clear')
 print("")
 print("|-------------------------------------------|")
-print("|    MagicStats v0.3.4 by Matz Trollmann    |")
+print("|    MagicStats v0.3.5 by Matz Trollmann    |")
 print("|  BTC: 3PBN9BHxFyjWoXBT1HH4YPDV5UcYBq9YsS  |")
 print("|  GIN: GgpRYX7NchKczJQs4CdE1yKhRSv9U8rL29  |")
 print("|  Github: https://github.com/Trollmann82/  |")
@@ -370,7 +370,7 @@ while True :
     # Prints data to screen every 5 minutes
     print("")
     print("|-------------------------------------------|")
-    print("|    MagicStats v0.3.4 by Matz Trollmann    |")
+    print("|    MagicStats v0.3.5 by Matz Trollmann    |")
     print("|  BTC: 3PBN9BHxFyjWoXBT1HH4YPDV5UcYBq9YsS  |")
     print("|  GIN: GgpRYX7NchKczJQs4CdE1yKhRSv9U8rL29  |")
     print("|  Github: https://github.com/Trollmann82/  |")
